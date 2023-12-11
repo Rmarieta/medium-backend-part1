be1ecbc639a2ee1254e8f255ba26586026279124
Rmarieta <raphael.marietan@hotmail.com>
2023-12-11 10:23:27 +0100

    Connecting database and defining routes

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
-# from flask_cors import CORS
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
 
     app = Flask(__name__)
-    # cors = CORS(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+        username=os.environ['RDS_USERNAME'],
+        password=os.environ['RDS_PASSWORD'],
+        host=os.environ['RDS_HOSTNAME'],
+        port=os.environ['RDS_PORT'],
+        database=os.environ['RDS_DB_NAME'],
+    )
+
+    # connect the app to the database
+    db.init_app(app)
 
     from app.views.main import main_bp
     app.register_blueprint(main_bp, url_prefix='/')
--- flask/app/views/main.py
+++ flask/app/views/main.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models.models import User
+from datetime import datetime
 
 main_bp = Blueprint('main', __name__)
 
@@ -7,14 +10,30 @@ main_bp = Blueprint('main', __name__)
 def EB_healthcheck():
     return 'OK', 200
 
+@main_bp.route('/register', methods=['POST'])
+def register_user():
+    try:
+        
+        data = request.get_json()
+        print('\n\nData\n\n',data,'\n\n')
+        new_user = User(
+            email = data["email"],
+            age = data["age"],
+            timestamp = datetime.utcnow()
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return 'User added', 200
+    except Exception as e:
+        db.session.rollback()
+        return f'An error occurred: {str(e)}', 500
 
-# @main_bp.route('/add', methods=['POST'])
-# def add():
-#     data = request.get_json()
 
-#     new_event = Event(
-#     )
-#     db.session.add(new_event)
-#     db.session.commit()
-
-#     return 'OK', 200
+@main_bp.route('/listusers', methods=['GET'])
+def list_users():
+    try:
+        users = db.session.query(User).all()
+        return jsonify([str(u) for u in users])
+    except Exception as e:
+        db.session.rollback()
+        return f'An error occurred: {str(e)}', 500
